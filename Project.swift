360ae5122131a7c3dcfbe83cf2951c4d59dba3dc
Bogdan Petkanych <bogdanpetkanych@gmail.com>
2026-05-04 19:15:26 +0200

    Introduce Project+Templates.swift

--- Project.swift
+++ Project.swift
@@ -1,7 +1,27 @@
 import ProjectDescription
+import ProjectDescriptionHelpers
 
-let project = Project(
+let settings: Settings = .settings(
+    base: [
+        "OTHER_LDFLAGS": ["$(inherited)", "-ObjC"],
+    ],
+    configurations: [
+        .debug(name: "Debug", xcconfig: "./xcconfigs/Flick-Project.xcconfig"),
+        .release(name: "Release", xcconfig: "./xcconfigs/Flick-Project.xcconfig"),
+    ]
+)
+
+
+let appSettings: Settings = .settings(
+    configurations: [
+        .debug(name: "Debug", xcconfig: "./xcconfigs/Flick.xcconfig"),
+        .release(name: "Release", xcconfig: "./xcconfigs/Flick.xcconfig"),
+    ]
+)
+
+let project = Project.app(
     name: "Flick",
+    organizationName: "urlaunched",
     packages: [
         .local(path: "./API"),
         .local(path: "./DesignSystem"),
@@ -9,39 +29,17 @@ let project = Project(
         .remote(url: "https://github.com/Maks-Jago/SwiftFoundation.git", requirement: .upToNextMajor(from: "0.3.6")),
         .remote(url: "https://github.com/Maks-Jago/SwiftUIKit", requirement: .upToNextMajor(from: "0.4.0")),
     ],
-    settings: .settings(
-        base: [
-            "OTHER_LDFLAGS": ["$(inherited)", "-ObjC"],
-        ],
-        configurations: [
-            .debug(name: "Debug", xcconfig: "./xcconfigs/Flick-Project.xcconfig"),
-            .release(name: "Release", xcconfig: "./xcconfigs/Flick-Project.xcconfig"),
-        ]
-    ),
-    targets: [
-        .target(
-            name: "Flick",
-            destinations: .iOS,
-            product: .app,
-            bundleId: "com.urlaunched.flick",
-            infoPlist: .file(path: "./Info.plist"),
-            sources: ["Flick/Sources/**"],
-            resources: ["Flick/Resources/**"],
-            dependencies: [
-                .external(name: "FlagKit", condition: nil),
-                .external(name: "SDWebImageSwiftUI", condition: nil),
-                .external(name: "UDF", condition: nil),
-                .package(product: "SwiftUI-Kit"),
-                .package(product: "SwiftFoundation"),
-                .package(product: "API"),
-                .package(product: "DesignSystem"),
-                .package(product: "Localizations"),
-            ],
-            settings: .settings(
-                configurations: [
-                    .debug(name: "Debug", xcconfig: "./xcconfigs/Flick.xcconfig"),
-                    .release(name: "Release", xcconfig: "./xcconfigs/Flick.xcconfig"),
-                ])
-        ),
-    ]
+    settings: settings,
+    appSettings: appSettings,
+    targetDependancies: [
+        .external(name: "FlagKit"),
+        .external(name: "SDWebImageSwiftUI"),
+        .external(name: "UDF"),
+        .package(product: "SwiftUI-Kit"),
+        .package(product: "SwiftFoundation"),
+        .package(product: "API"),
+        .package(product: "DesignSystem"),
+        .package(product: "Localizations"),
+    ],
+    moduleTargets: []
 )
---
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -0,0 +1,138 @@
+import ProjectDescription
+
+let featuresPath = "Features"
+let corePath = "Core"
+let appPath = "Flick"
+let reverseOrganizationName = "com.urlaunched"
+let infoPlistPath: Path = "Info.plist"
+
+public enum ModuleType {
+    case core
+    case feature
+    case app
+    
+    func path() -> String {
+        switch self {
+        case .core:
+            return corePath
+        case .feature:
+            return featuresPath
+        case .app:
+            return appPath
+        }
+    }
+}
+
+public struct Module {
+    let name: String
+    let path: String
+    let frameworkDependancies: [TargetDependency]
+    let frameworkResources: [String]
+    let moduleType: ModuleType
+    
+    public init(
+        name: String,
+        moduleType: ModuleType,
+        path: String,
+        frameworkDependancies: [TargetDependency],
+        frameworkResources: [String],
+        exampleResources: [String],
+        testResources: [String]
+    ) {
+        self.name = name
+        self.path = path
+        self.moduleType = moduleType
+        self.frameworkDependancies = frameworkDependancies
+        self.frameworkResources = frameworkResources
+    }
+}
+
+extension Project {
+    /// Helper function to create the Project for this ExampleApp
+    public static func app(
+        name: String,
+        organizationName: String,
+        packages: [Package] = [],
+        settings: Settings? = nil,
+        appSettings: Settings? = nil,
+        dependencies: [TargetDependency] = [],
+        targetDependancies: [TargetDependency] = [],
+        moduleTargets: [Module],
+    ) -> Project {
+        
+        let organizationName = organizationName
+        var targets = [Target]()
+        var dependencies = moduleTargets.map { TargetDependency.target(name: $0.name) }
+        dependencies.append(contentsOf: targetDependancies)
+        
+        let appTargets = makeAppTargets(
+            name: name,
+            dependencies: dependencies,
+            settings: appSettings
+        )
+        targets.append(contentsOf: appTargets)
+        
+        targets += moduleTargets.flatMap({ makeFrameworkTargets(module: $0) })
+        
+        
+        return Project(
+            name: name,
+            organizationName: organizationName,
+            packages: packages,
+            settings: settings,
+            targets: targets
+        )
+    }
+    
+    public static func makeFrameworkTargets(module: Module) -> [Target] {
+        let frameworkPath = "\(module.moduleType.path())/\(module.path)"
+        
+        let frameworkResourceFilePaths = module.frameworkResources.map {
+            ResourceFileElement.glob(pattern: Path(stringLiteral: "\(module.moduleType.path())/\(module.path)/" + $0), tags: [])
+        }
+        
+        
+        var targets = [Target]()
+        
+        let frameworkTarget = Target.target(
+            name: module.name,
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "\(reverseOrganizationName).\(module.name)",
+            infoPlist: .default,
+            sources: [
+                "\(frameworkPath)/Sources/**",
+            ],
+            resources: .resources(frameworkResourceFilePaths),
+            dependencies: module.frameworkDependancies,
+        )
+        
+        targets.append(frameworkTarget)
+        
+        return targets
+    }
+    
+    
+    public static func makeAppTargets(
+        name: String,
+        dependencies: [TargetDependency],
+        settings: Settings? = nil
+    ) -> [Target] {
+        var targets = [Target]()
+        
+        let mainTarget = Target.target(
+            name: name,
+            destinations: .iOS,
+            product: .app,
+            bundleId: "\(reverseOrganizationName).\(name)",
+            infoPlist: .file(path: infoPlistPath),
+            sources: ["\(appPath)/Sources/**"],
+            resources: ["\(appPath)/Resources/**"],
+            dependencies: dependencies,
+            settings: settings
+        )
+        
+        targets.append(mainTarget)
+        return targets
+    }
+}
