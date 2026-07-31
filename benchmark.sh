c1e2127125d2cd9be4dfac4612c196162c826f9b
Bogdan Petkanych <bogdanpetkanych@gmail.com>
2026-07-31 11:31:22 +0200

    Update config

--- benchmark.sh
+++ benchmark.sh
@@ -7,7 +7,7 @@ START_TIME=$(date +%s.%N)
 
 #️ Runs your commands seamlessly
 tuist install
-tuist cache warm --cache-profile only-external
+tuist cache warm --cache-profile only-external --print-hashes
 tuist generate --cache-profile only-external --no-open
 tuist test \
               --device="iPhone 17 Pro" \
